07b119a7bbf76c6b70053328dbba9980c771c577
Minh-Khoa Nguyen <khoanm206@gmail.com>
2022-12-23 15:51:58 +0700

    predict satellite functions

--- predictSat.h
+++ predictSat.h
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Sgp4.h>
 
 #define GMT_OFFSET_SECOND           7
 #define DAYLIGHT_OFFSET_SECOND      25200
@@ -6,3 +7,4 @@
 void getEpochTimeNow(unsigned long& epochTime);
 void updateEpochTimeNow(const char* ntpServerName, unsigned long& epochTime);
 unsigned long unixTimestamp(int year, int month, int day, int hour, int min, int sec);
+void initialize_Sat(String nameOfSat, Sgp4& sat, String payload);
