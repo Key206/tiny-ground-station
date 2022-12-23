2504989e506e5145b53b0ff02f5c20055e3af502
Minh-Khoa Nguyen <khoanm206@gmail.com>
2022-12-23 15:54:29 +0700

    main

--- tinyGS_ver02.ino
+++ tinyGS_ver02.ino
@@ -3,6 +3,7 @@
 #include <WiFiUdp.h>
 #include "TLEFunction.h"
 #include "predictSat.h"
+#include "controlStepper.h"
 
 #define SSID_WIFI                             "Thanh Tai"
 #define PASSWORD_WIFI                         "123456789"
@@ -10,15 +11,18 @@
 #define SERVER_TLE_TINYGS                     "https://api.tinygs.com/v1/tinygs_supported.txt"
 #define SERVER_NTP                            "pool.ntp.org"
 
+#define uS_TO_S_FACTOR                        1000000ULL  /* Conversion factor for micro seconds to seconds */
+#define TIME_PREPARE_AFTER_WAKEUP             20          /* in second */
+
+Sgp4 mySat;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 HTTPClient http;
 
+AccelStepper motorAz(MOTOR_INTERFACE_TYPE, STEP_AZ, DIR_AZ);
+AccelStepper motorEl(MOTOR_INTERFACE_TYPE, STEP_EL, DIR_EL);
 
-//String payload;
-static char tleLine1[70];
-static char tleLine2[70];
-
+String payload;
 unsigned long epochNow = 1660138928;
 
 void setup() {
@@ -27,15 +31,28 @@ void setup() {
   WiFi.begin(SSID_WIFI, PASSWORD_WIFI);
   while (WiFi.status() != WL_CONNECTED);
   Serial.println("WiFi connected.");
+  
   configTime(GMT_OFFSET_SECOND, DAYLIGHT_OFFSET_SECOND, SERVER_NTP);
   getEpochTimeNow(epochNow);
-  //Serial.println(epochNow);
-  //updateTleData(&http, payload, SERVER_TLE_TINYGS);
+  Serial.println(epochNow);
   
+  updateTleData(&http, payload, SERVER_TLE_TINYGS);
+  mySat.site(10.869,106.802,18);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("Hello world");
   delay(1000);
+}
+void goToSleep(unsigned int timeToSleep)
+{
+  esp_sleep_enable_timer_wakeup(timeToSleep * uS_TO_S_FACTOR);
+  Serial.println("Going to sleep now");
+  Serial.flush(); 
+  esp_deep_sleep_start();
+}
+unsigned int calculateSleepTime(unsigned long Now, unsigned long Start)
+{
+  return (unsigned int)(Start - Now - TIME_PREPARE_AFTER_WAKEUP);
 }
